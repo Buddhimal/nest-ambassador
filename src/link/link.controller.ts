c7578369c5df784628ce1e27377f9986014b5501
Buddhimal Gunasekara <bnawgunasekara@gmail.com>
2021-06-14 02:15:58 +0530

    stats done

--- src/link/link.controller.ts
+++ src/link/link.controller.ts
@@ -3,6 +3,9 @@ import {LinkService} from "./link.service";
 import {AuthGuard} from "../auth/auth.guard";
 import {AuthService} from "../auth/auth.service";
 import {Request} from "express";
+import {link} from "fs";
+import {Link} from "./link";
+import {Order} from "../order/order";
 
 @Controller()
 export class LinkController {
@@ -33,7 +36,29 @@ export class LinkController {
         return this.linkService.save({
             code: Math.random().toString(36).substr(6),
             user,
-            products: products.map(id => ({id}) )
+            products: products.map(id => ({id}))
+        })
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('ambassador/stats')
+    async stats(
+        @Req() request: Request
+    ) {
+        const user = await this.authService.user(request)
+
+        const links: Link[] = await this.linkService.find({
+            user,
+            relations: ['orders']
+        })
+
+        return links.map(link => {
+            const completedOrders: Order[] = link.orders.filter(o => o.complete)
+            return {
+                code: link.code,
+                count: completedOrders.length,
+                revenue: completedOrders.reduce((s, o) => s + o.ambassador_revenue, 0)
+            }
         })
     }
 
