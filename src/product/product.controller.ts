f62a5fd5da23af32be76f144cc0fd6fd6afa1246
Buddhimal Gunasekara <buddhimal.g@integritas.net>
2021-06-14 01:44:00 +0530

    products search, sort and pagination

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -7,7 +7,7 @@ import {
     Get, Inject,
     Param,
     Post,
-    Put,
+    Put, Req,
     UseGuards,
     UseInterceptors
 } from '@nestjs/common';
@@ -16,6 +16,8 @@ import {ProductCreateDto} from "./dtos/product-create.dto";
 import {AuthGuard} from "../auth/auth.guard";
 import {Cache} from "cache-manager";
 import {EventEmitter2} from "@nestjs/event-emitter";
+import {Request} from "express";
+import {Product} from "./product";
 
 @Controller()
 export class ProductController {
@@ -83,8 +85,10 @@ export class ProductController {
     }
 
     @Get('ambassador/product/backend')
-    async backend() {
-        let products = await this.cacheManager.get('products_backend1');
+    async backend(
+        @Req() request: Request
+    ) {
+        let products = await this.cacheManager.get<Product[]>('products_backend1');
 
         if (!products) {
             products = await this.productService.find({});
@@ -92,7 +96,34 @@ export class ProductController {
             await this.cacheManager.set('products_backend1', products, {ttl: 1800})
         }
 
-        return products;
+        if (request.query.s) {
+            const s = request.query.s.toString().toLowerCase();
+            products = products.filter(p => p.title.toLowerCase().indexOf(s) >= 0 || p.description.toLowerCase().indexOf(s) >= 0);
+        }
+
+        if (request.query.sort === 'asc' || request.query.sort === 'desc') {
+            products.sort((a, b) => {
+                const diff = a.price - b.price;
+                if (diff === 0) return 0;
+
+                const sign = Math.abs(diff) / diff;
+
+                return request.query.sort === 'asc' ? sign : -sign;
+            })
+        }
+
+        const page: number = parseInt(request.query.page as any) || 1;
+        const perPage = 9;
+        const total = products.length;
+
+        const data = products.slice((page - 1) * perPage, page * perPage);
+
+        return {
+            data,
+            total,
+            page,
+            last_page: Math.ceil(total/perPage)
+        };
     }
 
 
